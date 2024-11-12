5a9f9e04eb29b165fb4ee4fcfb467e1fa3151c27
Anirban Roy <aroyiem@gmail.com>
2024-11-12 15:57:54 +0000

    Used redux for state management

--- src/components/CartItems.tsx
+++ src/components/CartItems.tsx
@@ -1,9 +1,26 @@
-export default function CartItems() {
-  return (
-    <div id="cart">
-      <p>No items in cart!</p>
+import {useCartDispatch, useCartSelector} from "../store/hook.ts";
+import {addToCart, CartItem, removeFromCart} from "../store/cart-slice.ts";
 
-      {/* <ul id="cart-items">
+export default function CartItems() {
+
+    const cartItems = useCartSelector(state => state.cart.items);
+    const dispatch = useCartDispatch();
+    const totalPrice = cartItems.reduce((val, item) => val + (item.price * item.quantity), 0);
+    const formattedTotalPrice = totalPrice.toFixed(2);
+
+    function handleAddToCart(item: CartItem) {
+        dispatch(addToCart(item));
+    }
+
+    function handleRemoveFromCart(id: string) {
+        dispatch(removeFromCart(id));
+    }
+
+    return (
+    <div id="cart">
+        {cartItems.length === 0 && <p>No items in cart!</p>}
+
+      {cartItems.length > 0 &&  <ul id="cart-items">
           {cartItems.map((item) => {
             const formattedPrice = `$${item.price.toFixed(2)}`;
 
@@ -23,11 +40,11 @@ export default function CartItems() {
               </li>
             );
           })}
-        </ul> */}
+        </ul> }
 
-      {/* <p id="cart-total-price">
-        Cart Total: <strong>{formattedTotalPrice}</strong>
-      </p> */}
+      { <p id="cart-total-price">
+        Cart Total: <strong>£{formattedTotalPrice}</strong>
+      </p> }
     </div>
   );
 }
